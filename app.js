369f39dd90e507ea30a74130029faad792525f7e
stornhp <38326039+stornhp@users.noreply.github.com>
2018-05-06 13:55:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,50 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const fs = require("fs");
-const moment = require("moment");
+const Discord = require('discord.js');
 
-//JSON Files
-let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
+var bot = new Discord.Client();
+
+const PREFIX = "!";
+
+function random(min, max){
+    min = Math.ceil(0);
+    max = Math.floor(4);
+    randnum = Math.floor(Math.random() * (max - min +1)+ min);
+}
 
 bot.on('ready', () => {
-    console.log(`Je suis connecté sous le pseudonyme: ${bot.user.tag} !`);
+    console.log(`Connecté sous le pseudonyme: ${bot.user.tag}!`);
 });
 
 bot.on('message', message => {
-    let sender = message.author;
-    let prefix = "!"
-    if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
-    if (!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = 150.
-    if (!userData[sender.id + message.guild.id].lastDaily) userData[sender.id + message.guild.id].lastDaily = "Not Collected";
+    const guildMember = message.member;
+    if(message.content[0] === PREFIX) {
+        if(message.content === "!choipeaux") {
+            random()
+            if (randnum ==1) {
+                message.reply("Sage es-tu... Je ne vois qu'une option... SERDAIGLE !");
+                guildMember.addRole(process.env.SERDAIGLE)
+                console.log(`${message.author.username} est devenu Serdaigle.`);
+            }
 
-    fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
-        if (err) console.error (err);
-    })
-    
-    if (message.content === prefix + "ping") {
-        message.channel.send("Pong")
+            if (randnum ==2) {
+                message.reply("Ambitieux est-tu... Seul... SERPENTARD pourras t'aider à grandir sur le chemin de la Puissance Ultime !");
+                guildMember.addRole(process.env.SERPENTARD)
+                console.log(`${message.author.username} est devenu Serpentard.`);
+            }
+
+            if (randnum ==3) {
+                message.reply("Courageux es-tu... Je vois la seule maison qui t'acceptera... GRYFFONDOR !");
+                guildMember.addRole(process.env.GRYFFONDOR)
+                console.log(`${message.author.username} est devenu Gryffondor.`);
+            }
+
+            if (randnum ==4) {
+                message.reply("Modeste es-tu... Je crois, que seul POUFSOUFFLE aura le charme de te faire aimer Poudlard !")
+                guildMember.addRole(process.env.POUFSOUFFLE)
+                console.log(`${message.author.username} est devenu Poufsouffle`);
+            }
+        }
     }
-
-    if (message.content === prefix + "gringotts" || message.content === prefix + "balance") {
-        message.channel.send({embed:{
-            title: "Gringotts",
-            color: 0x0079FF,
-            fields: [{
-                name: "Propriétaire du Compte",
-                value: message.author.username,
-                inline: true
-            },
-            {
-                name: "Balance du Compte",
-                value:userData[sender.id + message.guild.id].money,
-                inline: true
-            }]
-        }})
-    }
-
-    if (message.content === prefix + "paye" || message.content === prefix + "daily") {
-        if (userData[sender.id + message.guild.id].lastDaily != moment().format('L')) {
-            userData[sender.id + message.guild.id].lastDaily = moment().format('L')
-            userData[sender.id + message.guild.id].money += 500;
-            message.channel.send({embed: {
-                title:'Jour de Paye',
-                color: 0x0079FF,
-                description:'Vous récoltez 50 Gallions, ajouté à votre account!'
-                
-            }})
-        } else {
-            message.channel.send({embed: {
-                title:'Jour de Paye',
-                color: 0x0079FF,
-                description:'Vous avez déjà récolté votre paye. Vous pouvez récolter votre prochaine paye dans' + moment().endOf('jour').fromNow() + '.'
-        }})
-    }
-
-    fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
-        if (err) console.error(err);
-    })
-}});
+});
 
 bot.login(process.env.TOKEN);
+
